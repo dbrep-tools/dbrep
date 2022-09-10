7894843760797afe71b4888e7e9601635b7afdb0
Valentin Stepanovich <kolhizin@gmail.com>
2022-09-10 17:42:12 +0000

    Added performance tests vs pandas & airbyte

---
+++ tests/performance/test_sql_single_col.py
@@ -0,0 +1,101 @@
+from base.sql_test import SQLPerformanceTest
+import dbrep
+import dbrep.replication
+from dbrep.engines.engine_sqlalchemy import SQLAlchemyEngine
+import pandas as pd
+import sqlalchemy
+import time
+import sys
+
+class SingleColSQLTest(SQLPerformanceTest):
+    def __init__(self, src_conn_str, dst_conn_str, src_type, dst_type, num_rows, values) -> None:
+        self.src_table = 'test_single_col_src'
+        self.dst_table = 'test_single_col_dst'
+        super().__init__(src_conn_str, dst_conn_str, src_tables=[self.src_table], dst_tables=[self.dst_table])
+        self.src_type = src_type
+        self.dst_type = dst_type
+        self.num_rows = num_rows
+        self.values = values
+
+    def setup(self):
+        try:
+            self.src_conn.execute('create table {} (rid serial, col {})'.format(self.src_table, self.src_type))
+            self.dst_conn.execute('create table {} (rid integer, col {})'.format(self.dst_table, self.dst_type))
+        except Exception as e:
+            self.cleanup()
+            
+        batch_size = 100
+        for k in range(0, self.num_rows, batch_size):
+            act_size = min(k+batch_size, self.num_rows) - k
+            act_vals = ['({})'.format(self.values[i % len(self.values)]) for i in range(act_size)]
+            query = 'insert into {} (col) values {}'.format(self.src_table, ','.join(act_vals))
+            self.src_conn.execute(query)
+
+    def reset(self):
+        self.dst_conn.execute('truncate table {}'.format(self.dst_table))
+
+
+def run_dbrep_sqla_full_copy(test):
+    src = SQLAlchemyEngine({'conn-str':test.src_conn_str})
+    dst = SQLAlchemyEngine({'conn-str':test.dst_conn_str})
+    dbrep.replication.full_copy(src, dst, {
+        'src': {'table': test.src_table, 'rid': 'rid'},
+        'dst': {'table': test.dst_table, 'rid': 'rid'}
+    })
+
+
+def run_dbrep_sqla_increment(test):
+    src = SQLAlchemyEngine({'conn-str':test.src_conn_str})
+    dst = SQLAlchemyEngine({'conn-str':test.dst_conn_str})
+    dbrep.replication.incremental_update(src, dst, {
+        'src': {'table': test.src_table, 'rid': 'rid'},
+        'dst': {'table': test.dst_table, 'rid': 'rid'}
+    })
+
+def run_pandas(test):
+    src_engine = sqlalchemy.create_engine(test.src_conn_str)
+    dst_engine = sqlalchemy.create_engine(test.dst_conn_str)
+    df = pd.read_sql(test.src_table, src_engine)
+    df[['col']].to_sql(test.dst_table, dst_engine, if_exists='append', index=False)
+
+
+if __name__ == '__main__':
+    test = SingleColSQLTest(
+        'postgresql+psycopg2://postgres:postgres@postgres:5432/postgres',
+        'postgresql+psycopg2://postgres:postgres@postgres:5432/postgres',
+        'varchar(40)',
+        'varchar(40)',
+        1000000,
+        ["'abcdefghj'", "'1234567'"]
+    )
+    print('Running setup...')
+    test.setup()
+    
+    if len(sys.argv) >= 2 and sys.argv[1] == 'setup':
+        print('Setup complete.')
+    else:
+        print('Running reset...')
+        test.reset()
+        print('Running replication...')
+        t0 = time.time()
+        run_dbrep_sqla_full_copy(test)
+        t1 = time.time()
+        print('run_dbrep_sqla_full_copy: {:.2f}'.format(t1-t0))
+        
+        print('Running reset...')
+        test.reset()
+        print('Running replication...')
+        t0 = time.time()
+        run_dbrep_sqla_increment(test)
+        t1 = time.time()
+        print('run_dbrep_sqla_increment: {:.2f}'.format(t1-t0))
+        
+        print('Running reset...')
+        test.reset()
+        print('Running replication...')
+        t0 = time.time()
+        run_pandas(test)
+        t1 = time.time()
+        print('run_pandas: {:.2f}'.format(t1-t0))
+
+        test.cleanup()
