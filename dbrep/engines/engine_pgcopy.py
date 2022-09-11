925bec7c56a78c89be63a846d961e588400c4ace
Valentin Stepanovich <kolhizin@gmail.com>
2022-09-11 17:05:06 +0000

    Implemented experimental pgcopy

--- dbrep/engines/engine_pgcopy.py
+++ dbrep/engines/engine_pgcopy.py
@@ -1,5 +1,6 @@
 
 from io import StringIO
+from re import template
 
 from .engine_dbapi import DBAPIEngine
 from .. import add_engine_factory
@@ -8,20 +9,21 @@ class PGCopyEngine(DBAPIEngine):
     id = 'pgcopy'
     
     def __init__(self, connection_config):     
-        print('Warning! This is experimental feature!')
+        print('Warning! This is experimental feature! Does not work with timestamps, dates, special characters in strings!')
         connection_config['driver'] = 'psycopg2'
         super().__init__(connection_config)
 
     def insert_batch(self, names, batch):
         buffer = StringIO()
 
-        header = ';'.join(names)
-        body = [';'.join([str(y) for y in x]) for x in batch]
-        buffer.writelines([header] + body)
-        buffer.seek(0)
+        template = ';'.join(['%s']*len(names))
 
         with self.conn.cursor() as cur:
-            cur.copy_from(buffer, self.active_insert, sep=";")
+            body = [cur.mogrify(template, x).decode('utf8') for x in batch]
+            buffer.write('\n'.join(body))
+            buffer.seek(0)
+
+            cur.copy_from(buffer, self.active_insert, sep=";", null='NULL')
         self.conn.commit()
 
 add_engine_factory(PGCopyEngine.id, PGCopyEngine)
